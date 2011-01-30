033671df0e3cac8d9ea93a33ac03947e360fc007
Rafael Fernández López <ereslibre@ereslibre.es>
2011-01-30 20:50:55 +0100

    Initial commit of culturify-www. Raw rails new command.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CulturifyWww::Application.config.secret_token = '9d646ba952086027f934d183779c389be25423da948b98900b19fd2b8d2c34b254618070d3aa3fa96c96ef88ace45557c1b58bae06f04bf6de2c135343672989'
